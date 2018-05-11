9575eca9d697e9a757faa2307f2f2700608e1525
taodaren_workspace <taoyuchen@github.com>
2018-05-11 11:08:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EJColorFlower
+# EJColorFlower
---
+++ app/src/main/java/cn/eejing/ejcolorflower/MainActivity.java
@@ -0,0 +1,126 @@
+package cn.eejing.ejcolorflower;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
+    private ArrayList<Fragment> mFragments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        initBtnNavBar();
+        mFragments = getFragments();
+        setDefFragment();
+    }
+
+    /**
+     * 设置底部导航
+     */
+    private void initBtnNavBar() {
+        BottomNavigationBar mNavBar = findViewById(R.id.bottom_navigation_bar);
+
+        // 设置模块名背景色
+        mNavBar.setBarBackgroundColor(R.color.colorPrimary);
+        // 设置背景模式
+        mNavBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_RIPPLE);
+        // 设置Tab点击的模式
+        mNavBar.setMode(BottomNavigationBar.MODE_FIXED);
+
+        // 添加 Tab
+        mNavBar
+                // 设置导航图标及名称
+                .addItem(new BottomNavigationItem(R.drawable.tab_device, R.string.device_name)
+                        // 导航背景颜色
+                        .setActiveColorResource(R.color.colorWhite))
+                .addItem(new BottomNavigationItem(R.drawable.tab_control, R.string.control_name)
+                        .setActiveColorResource(R.color.colorWhite))
+                .addItem(new BottomNavigationItem(R.drawable.tab_mall, R.string.mall_name)
+                        .setActiveColorResource(R.color.colorWhite))
+                .addItem(new BottomNavigationItem(R.drawable.tab_mine, R.string.mine_name)
+                        .setActiveColorResource(R.color.colorWhite))
+                // 默认显示面板
+                .setFirstSelectedPosition(0)
+                // 初始化
+                .initialise();
+
+        // 设置事件监听器
+        mNavBar.setTabSelectedListener(this);
+    }
+
+    /**
+     * 设置默认 fragment
+     */
+    private void setDefFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.main_content, TabDeviceFragment.newInstance());
+        transaction.commit();
+    }
+
+    /**
+     * 将 Fragment 加入 fragments 里面
+     */
+    private ArrayList<Fragment> getFragments() {
+        ArrayList<Fragment> list = new ArrayList<>();
+        list.add(TabDeviceFragment.newInstance());
+        list.add(TabControlFragment.newInstance());
+        list.add(TabMallFragment.newInstance());
+        list.add(TabMineFragment.newInstance());
+        return list;
+    }
+
+    /**
+     * Tab 被选中
+     */
+    @Override
+    public void onTabSelected(int position) {
+        // 点击时加载对应的 fragment
+        if (mFragments != null) {
+            if (position < mFragments.size()) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragment = mFragments.get(position);
+                transaction.replace(R.id.main_content, fragment);
+                transaction.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    /**
+     * Tab 被取消选中
+     */
+    @Override
+    public void onTabUnselected(int position) {
+        if (mFragments != null) {
+            if (position < mFragments.size()) {
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                Fragment fragment = mFragments.get(position);
+                transaction.remove(fragment);
+                transaction.commitAllowingStateLoss();
+            }
+        }
+    }
+
+    /**
+     * Tab 被重新选中
+     */
+    @Override
+    public void onTabReselected(int position) {
+    }
+
+}
