c470bb70b2ba019f70708f4f49cf2d3c51e1c921
taodaren <397771543@qq.com>
2018-06-14 18:01:03 +0800

    pay ing...

--- app/src/main/java/cn/eejing/ejcolorflower/ui/activity/PaymentOrderActivity.java
+++ app/src/main/java/cn/eejing/ejcolorflower/ui/activity/PaymentOrderActivity.java
@@ -3,6 +3,8 @@ package cn.eejing.ejcolorflower.ui.activity;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.allen.library.SuperButton;
+
 import butterknife.BindView;
 import cn.eejing.ejcolorflower.R;
 import cn.eejing.ejcolorflower.ui.base.BaseActivity;
@@ -15,6 +17,8 @@ public class PaymentOrderActivity extends BaseActivity implements View.OnClickLi
 
     @BindView(R.id.img_title_back)
     ImageView imgTitleBack;
+    @BindView(R.id.btn_pay)
+    SuperButton btnPay;
 
     @Override
     protected int layoutViewId() {
@@ -29,6 +33,7 @@ public class PaymentOrderActivity extends BaseActivity implements View.OnClickLi
     @Override
     public void initListener() {
         imgTitleBack.setOnClickListener(this);
+        btnPay.setOnClickListener(this);
     }
 
     @Override
@@ -37,9 +42,17 @@ public class PaymentOrderActivity extends BaseActivity implements View.OnClickLi
             case R.id.img_title_back:
                 finish();
                 break;
+            case R.id.btn_pay:
+                // pay
+                getOrderInfo();
+                break;
             default:
                 break;
         }
+    }
+
+    private void getOrderInfo() {
+        // TODO: 2018/6/14  生成订单信息接口
     }
 
 }
