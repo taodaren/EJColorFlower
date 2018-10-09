549f3e9129d10e26f96f7568cad69e72f89de4f1
taodaren <397771543@qq.com>
2018-10-09 10:29:15 +0800

    商城首页布局更新

--- app/src/main/java/cn/eejing/ejcolorflower/view/adapter/TabMallAdapter.java
+++ app/src/main/java/cn/eejing/ejcolorflower/view/adapter/TabMallAdapter.java
@@ -2,8 +2,9 @@ package cn.eejing.ejcolorflower.view.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,15 +20,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.eejing.ejcolorflower.R;
-import cn.eejing.ejcolorflower.view.activity.MainActivity;
 import cn.eejing.ejcolorflower.model.request.GoodsListBean;
-import cn.eejing.ejcolorflower.view.activity.MaGoodsDetailsActivity;
 
 /**
- * @创建者 Taodaren
  * 实现 Adapter 步骤：
  * 写 ViewHolder → extends RecyclerView.Adapter<泛型是写的VH> → 全局变量与构造方法（Context,list,LayoutInflater）→ 完善重写方法
  */
+
 public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallViewHolder> {
     private Context mContext;
     private List<GoodsListBean.DataBean> mList;
@@ -48,6 +47,7 @@ public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallView
         return mallViewHolder;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onBindViewHolder(@NonNull MallViewHolder holder, int position) {
         holder.setData(mList.get(position));
@@ -58,18 +58,14 @@ public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallView
         return mList.size();
     }
 
-    /**
-     * 刷新列表
-     */
+    /** 刷新列表 */
     public void refreshList(List<GoodsListBean.DataBean> list) {
         // 先把之前的数据清空
         mList.clear();
         addList(list);
     }
 
-    /**
-     * 加载更多列表
-     */
+    /** 加载更多列表 */
     public void addList(List<GoodsListBean.DataBean> list) {
         // 把新集合添加进来
         mList.addAll(list);
@@ -87,13 +83,13 @@ public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallView
 
         View outView;
 
-        public MallViewHolder(View itemView) {
+        MallViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             outView = itemView;
         }
 
-        public void setClickListener() {
+        void setClickListener() {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -110,6 +106,7 @@ public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallView
             });
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.M)
         @SuppressLint("SetTextI18n")
         public void setData(GoodsListBean.DataBean bean) {
             Glide.with(mContext).load(bean.getImage()).into(imgGoods);
@@ -117,14 +114,14 @@ public class TabMallAdapter extends RecyclerView.Adapter<TabMallAdapter.MallView
             tvRmb.setText(mContext.getResources().getString(R.string.rmb) + bean.getMoney());
             tvNum.setText(mContext.getResources().getString(R.string.sold) + bean.getSold());
             int stock = bean.getStock();
-            switch (stock) {
-                case 1:
-                    tvHas.setText(mContext.getResources().getString(R.string.stock_have));
-                    break;
-                case 0:
-                    tvHas.setText(mContext.getResources().getString(R.string.stock_no));
-                    break;
-                default:
+            if (stock == 0) {
+                // 无货
+                tvHas.setVisibility(View.VISIBLE);
+                tvHas.setText(mContext.getResources().getString(R.string.stock_no));
+                tvHas.setTextColor(mContext.getColor(R.color.colorRmb));
+                tvTitle.setTextColor(mContext.getColor(R.color.colorNoClick));
+                tvRmb.setTextColor(mContext.getColor(R.color.colorNoClick));
+                tvNum.setTextColor(mContext.getColor(R.color.colorNoClick));
             }
         }
     }
