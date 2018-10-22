9af14f365257ca4070198605dff772d49f2228d3
taodaren <397771543@qq.com>
2018-10-22 14:09:59 +0800

    修改添加喷射效果界面UI

--- app/src/main/java/cn/eejing/ejcolorflower/view/activity/CtAddEffectActivity.java
+++ app/src/main/java/cn/eejing/ejcolorflower/view/activity/CtAddEffectActivity.java
@@ -8,6 +8,10 @@ import cn.eejing.ejcolorflower.R;
 import cn.eejing.ejcolorflower.app.AppConstant;
 import cn.eejing.ejcolorflower.view.base.BaseActivity;
 
+/**
+ * 添加喷射效果界面
+ */
+
 public class CtAddEffectActivity extends BaseActivity {
 
     @Override
@@ -20,7 +24,7 @@ public class CtAddEffectActivity extends BaseActivity {
         setToolbar("添加效果", View.VISIBLE, null, View.GONE);
     }
 
-    @OnClick({R.id.add_ride_effect, R.id.add_stream_effect, R.id.add_together_effect, R.id.add_interval_effect})
+    @OnClick({R.id.add_ride_effect, R.id.add_stream_effect, R.id.add_together_effect, R.id.add_interval_effect, R.id.add_stop_jet})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.add_ride_effect:
@@ -37,6 +41,10 @@ public class CtAddEffectActivity extends BaseActivity {
                 break;
             case R.id.add_interval_effect:
                 setResult(RESULT_OK, new Intent().putExtra("jet_mode", AppConstant.CONFIG_INTERVAL));
+                finish();
+                break;
+            case R.id.add_stop_jet:
+                setResult(RESULT_OK, new Intent().putExtra("jet_mode", AppConstant.CONFIG_STOP));
                 finish();
                 break;
         }
