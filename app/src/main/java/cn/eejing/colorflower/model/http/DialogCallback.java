1c5c06141ca5da31fa76b0a181f3a27d58fbd49a
taodaren <397771543@qq.com>
2018-12-29 12:24:46 +0800

    去掉网络请求 progressDialog 提示

--- app/src/main/java/cn/eejing/colorflower/model/http/DialogCallback.java
+++ app/src/main/java/cn/eejing/colorflower/model/http/DialogCallback.java
@@ -8,12 +8,12 @@ import com.lzy.okgo.model.Response;
 import com.lzy.okgo.request.base.Request;
 
 /**
- * 网络请求前显示一个 dialog,请求结束后取消 loading
+ * 网络请求前显示一个 dialog ,请求结束后取消 loading
  */
 
 public abstract class DialogCallback<T> extends JsonCallback<T> {
 
-    private ProgressDialog mProgressDialog;
+//    private ProgressDialog mProgressDialog;
 
     public DialogCallback(Activity activity, Class<T> tClass) {
         super(tClass);
@@ -21,36 +21,37 @@ public abstract class DialogCallback<T> extends JsonCallback<T> {
     }
 
     private void initDialog(Activity activity) {
-        mProgressDialog = new ProgressDialog(activity);
-        mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mProgressDialog.setCanceledOnTouchOutside(false);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        mProgressDialog.setMessage("网络请求中");
+//        mProgressDialog = new ProgressDialog(activity);
+//        mProgressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        mProgressDialog.setCanceledOnTouchOutside(false);
+//        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//        mProgressDialog.setMessage("网络请求中");
     }
 
     @Override
     public void onStart(Request<T, ? extends Request> request) {
-        if (mProgressDialog != null && !mProgressDialog.isShowing()) {
-            try {
-                mProgressDialog.show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+//        if (mProgressDialog != null && !mProgressDialog.isShowing()) {
+//            try {
+//                mProgressDialog.show();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     @Override
     public void onFinish() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            try {
-                mProgressDialog.dismiss();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+//        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+//            try {
+//                mProgressDialog.dismiss();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     @Override
     public void onSuccess(Response<T> response) {
     }
+
 }
